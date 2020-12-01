e294066f431a6a6573a57b093bc24f2a00a6ccc8
Justin Yun <justinyun@google.com>
2020-12-01 17:10:07 +0900

    Update VNDK snapshot v30 headers for x86
    Taken from branch rvc-gsi-release (6980711).
    This adds the header files for the VNDK snapshots.
    
    Test: build
    Bug: 174323911
    Bug: 150902910
    Change-Id: I04a1eee8847db96455045e5ad5541b066ad7f032

--- x86/include/out/soong/.intermediates/hardware/interfaces/automotive/evs/1.1/android.hardware.automotive.evs@1.1_genc++_headers/gen/android/hardware/automotive/evs/1.1/types.h
+++ x86/include/out/soong/.intermediates/hardware/interfaces/automotive/evs/1.1/android.hardware.automotive.evs@1.1_genc++_headers/gen/android/hardware/automotive/evs/1.1/types.h
@@ -289,33 +289,6 @@ static_assert(__alignof(::android::hardware::automotive::evs::V1_1::Translation)
  * Provides the orientation and location of a car sensor relative to the android automotive
  * coordinate system:
  * https://source.android.com/devices/sensors/sensor-types#auto_axes
- * The sensor pose defines the transformation to be applied to the android automotive axes to
- * obtain the sensor local axes.
- * The pose consists of rotation, (specified as a quaternions) and translation
- * (vector with x, y, z).
- * This rotation and translation applied to the sensor data in the sensor's local coordinate
- * system transform the data to the automotive coordinate system.
- * i.e   Pcar =  ( Rot * Psensor ) + Trans
- * Here Pcar is a point in automotive coordinate system and Psensor is a point in the sensor's
- * coordinate system.
- * Example:
- * For a sensor on the front bumper and on the left corner of the car with its X axis pointing to
- * the front, the sensor is located at (-2, 4, 0) meters w.r.t android automotive axes and the
- * sensor local axes has a rotation of 90 degrees counter-clockwise w.r.t android automotive axes
- * when viewing the car from top on the +Z axis side:
- *
- *      ↑X sensor
- *    Y←∘______
- *      |      |  front
- *      | car  |
- *      |  ↑Y  |
- *      |  ∘→X |  rear
- *      |______|
- *
- * For this example the rotation and translation will be:
- * Rotation = + 90 degrees around Z axis = (0.7071, 0, 0, 0.7071) as a unit quaternion.
- * Translation = (-2, 4, 0) in meters = (-2000, 4000, 0) in milli-meters.
- * Note: Every sensor type must specify its own pose.
  */
 struct SensorPose final {
     /**
